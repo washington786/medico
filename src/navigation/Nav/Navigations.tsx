0d68a9bf003d2cc09e84dfe000c7303d1361f081
washington786 <dkmawasha@gmail.com>
2023-04-24 19:33:05 +0200

    firebase and front-end

--- src/navigation/Nav/Navigations.tsx
+++ src/navigation/Nav/Navigations.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
-
 import { NavigationContainer } from '@react-navigation/native';
-import Auth from '../stacks/Auth';
-import BottomNavigation from '../bottom/BottomNavigation';
 import MainStack from '../stacks/MainStack';
 
 const Navigations = () => {
   return (
     <NavigationContainer>
-        {/* <Auth/>
-        <BottomNavigation/> */}
         <MainStack/>
     </NavigationContainer>
   )

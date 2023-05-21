3ea672c2daa39c29abd674aeffd3fb1026b6199a
washington786 <dkmawasha@gmail.com>
2023-05-22 00:04:31 +0200

    front-end

--- src/screens/Main/Info/Info.tsx
+++ src/screens/Main/Info/Info.tsx
@@ -12,7 +12,6 @@ const Info = () => {
     var found = instructions.find((info) => {
       return info.id === id;
     });
-    console.log(found);
     if (found) {
       setIsActive(!isActive);
     } else {
